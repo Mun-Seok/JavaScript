761962934179e4c67fc76541b931bbf453afdeb3
ChoiMunSeok <105057510+Moon-0311@users.noreply.github.com>
2022-05-27 14:07:55 +0900

    script 06

--- JS/s06.js
+++ JS/s06.js
@@ -4,15 +4,17 @@ function plus(i) {
 plus(100);
 console.log('');
 function welcome(name) {
-    console.log('안녕하세요, ' + name + '님!');
+    console.log('안녕하세요,' + name + '님!');
 }
-welcome('허지훈');
-welcome('장소영');
-welcome('최이서');
 welcome('최문석');
 welcome(1004);
 console.log('');
-
+function welcome2() {
+    console.log('안녕하세요, 최문석님');
+}
+welcome2();
+welcome2();
+console.log('');
 function printSquare(x) {
     console.log(x * x);
 }
@@ -20,49 +22,37 @@ printSquare(2);
 printSquare(15);
 printSquare(132);
 console.log('');
-
-/* 메모리 용량 계산식 만들기 */
-/* 함수 2개, teraToGiga, teraToMega
+/*메모리 용량 계산식 만들기 */
+/*
+함수 두개, teraToGiga, teraToMega
 테라바이트를 기가바이트로 환산,
 테라바이트를 메가바이트로 환산
-3테라바이트를 환산하기
+3테라바이트를 환산
 */
-function teraToGiga(i) {
-    let gigaByte = i * 1024;
-    console.log(i + 'TB는 ');
-    console.log(gigaByte + 'GB입니다.');
+function teraToGiga(a) {
+    console.log(a + 'TB는');
+    console.log(a * 1024 + 'GB입니다.');
 }
-function teraToMega(i) {
-    let megaByte = i * 1024 * 1024;
-    console.log(i + 'TB는 ');
-    console.log(megaByte + 'MB입니다.');
+function teraToMega(a) {
+    console.log(a + 'TB는');
+    console.log(a * 1024 * 1024 + 'MB입니다.');
 }
 teraToGiga(3);
 teraToMega(3);
+console.log('');
 /*
 1TB는
-1024GB입니다
+1024GB입니다.
 */
-console.log('');
-
 function printSum(a, b) {
     console.log(a + b);
 }
 printSum(100, 100);
 printSum(50, 130);
-
-console.log('');
 /*
 함수 introduce를 정의하고,
-매개변수 name, birth, national, job을 사용
-
-콘솔창 예시
-안녕하세요! 반갑습니다.
-제 이름은 name입니다.
-생년월일은 birth이고,
-국적은 nation이고,
-직업은 job입니다.
-잘부탁드립니다!
+매개변수로 name, birth, national, job을 사용해서
+콘솔창에
 */
 function introduce(name, birth, national, job) {
     console.log('안녕하세요! 반갑습니다.');
@@ -70,27 +60,16 @@ function introduce(name, birth, national, job) {
     console.log('생년월일은 ' + birth + '이고,');
     console.log('국적은 ' + national + '이고,');
     console.log('직업은 ' + job + '입니다.');
-    console.log('잘부탁드립니다!');
+    console.log('잘 부탁드립니다!');
 }
-
 introduce('김철수', '1998.01.15', '한국', '회사원');
-console.log('');
 introduce('이영희', '1988.03.15', '중국', '무역 회사원');
 console.log('');
-
-/* bmi지수 */
-/* 
-bmi = 무게 / ((키*키) /10000)
-
-콘솔창 예시
-홀쭉이 정보 : 몸무게  42.1, 키 165
-홀쭉이님의 체질량 지수는 ㅁㅁㅁ입니다.
+/*
+bmi =무게 / ((키* 키) / 10000)
 */
-function bmi(name, weight, height) {
-    let rate = weight / ((height * height) / 10000);
-    console.log(name + ' 정보 : 몸무게 ' + weight + ' 키 ' + height);
-    console.log(name + '님의 체질량 지수는 ' + rate + '입니다.');
+function bmi(name, weight, tall) {
+    let bmi = weight / ((tall * tall) / 10000);
+    console.log(name + '님의 체질량 지수는' + bmi + '입니다.');
 }
 bmi('홀쭉이', 42.1, 165);
-console.log('');
-bmi('통통이', 75, 159);
